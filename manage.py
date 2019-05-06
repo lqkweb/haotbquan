c60edf36d444d7f9b8c8c08b240bf729158a6451
leiqk <leiqk@dxy.cn>
2019-05-06 11:37:40 +0800

    [init]初始化代码

---
+++ manage.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python  
+# encoding: utf-8  
+
+""" 
+@author: lqk  
+@contact: 798244092@qq.com 
+@site: https://github.com/lqkweb 
+@file: manage.py 
+@time: 2019/4/28 3:23 PM 
+"""
+from flask import Flask,render_template
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
